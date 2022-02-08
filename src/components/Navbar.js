481395ab796ddfb4439630651dd8c13ee8cd687b
rVnPower <rvnpower@protonmail.com>
2022-02-08 21:04:27 +0700

    added components

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <a className='pageName' href="#">Demon List VN</a>
+      <div className='navItem'>
+        <ul>
+          <li><a href="#">Main List</a></li>
+          <li><a href="#">Legacy List</a></li>
+          <li><a href="#">Top Player</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ src/components/Side.js
@@ -0,0 +1,13 @@
+
+function Side() {
+  return (
+        <div className='sidePanel'>
+          <div className='topplayerPanel'>
+            <h2>Top Player</h2>
+          </div>
+        </div>
+  )
+}
+
+export default Side
+
