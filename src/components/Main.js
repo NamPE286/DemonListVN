5278dea3f6408097a059900a16d8366a8c654ebb
NamPE286 <nambuihung654@gmail.com>
2022-02-09 00:37:27 +0700

    fixed navbar and side panel

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,6 +56,34 @@ Main.defaultProps = [
     creator: "Hololive JP",
     point: "1000"
   },
+  {
+    thumbnail: "https://i.imgur.com/BdBy1Ky.png",
+    top: 5,
+    name: "Rushia",
+    creator: "Hololive JP",
+    point: "1000"
+  },
+  {
+    thumbnail: "https://c4.wallpaperflare.com/wallpaper/262/965/121/gawr-gura-anime-girls-water-hd-wallpaper-preview.jpg",
+    top: 6,
+    name: "Gawr Gura",
+    creator: "Hololive EN",
+    point: "1000"
+  },
+  {
+    thumbnail: "https://wallpaperaccess.com/full/6391518.jpg",
+    top: 7,
+    name: "Watson Amelia",
+    creator: "Hololive EN",
+    point: "1000"
+  },
+  {
+    thumbnail: "https://wallpaperaccess.com/full/4858745.jpg",
+    top: 8,
+    name: "Minato Aqua",
+    creator: "Hololive JP",
+    point: "1000"
+  },
 ];
 
 export default Main;
