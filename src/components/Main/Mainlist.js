8b38eca234b74a9f2c311d429505752d5b89c0c4
rVnPower <rvnpower@protonmail.com>
2022-02-09 19:51:50 +0700

    using math formula to calculate the point instead of storing

--- src/components/Main/Mainlist.js
+++ src/components/Main/Mainlist.js
@@ -15,7 +15,7 @@ function Main(levels) {
                 <h3>{levels[i].name}</h3>
                 <p>by {levels[i].creator}</p>
                 <br></br>
-                <p>{levels[i].point}pt</p>
+                <p>{+(2100/(0.3*levels[i].top+9)-80).toFixed(1)}pt</p>
               </div>
             </div>
           )
@@ -33,56 +33,47 @@ Main.defaultProps = [
     top: 1,
     name: "Rushia",
     creator: "Hololive JP",
-    point: "1000"
   },
   {
     thumbnail: "https://c4.wallpaperflare.com/wallpaper/262/965/121/gawr-gura-anime-girls-water-hd-wallpaper-preview.jpg",
     top: 2,
     name: "Gawr Gura",
     creator: "Hololive EN",
-    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/6391518.jpg",
     top: 3,
     name: "Watson Amelia",
     creator: "Hololive EN",
-    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/4858745.jpg",
     top: 4,
     name: "Minato Aqua",
     creator: "Hololive JP",
-    point: "1000"
-  },
   {
     thumbnail: "https://i.imgur.com/BdBy1Ky.png",
     top: 5,
     name: "Rushia",
     creator: "Hololive JP",
-    point: "1000"
   },
   {
     thumbnail: "https://c4.wallpaperflare.com/wallpaper/262/965/121/gawr-gura-anime-girls-water-hd-wallpaper-preview.jpg",
     top: 6,
     name: "Gawr Gura",
     creator: "Hololive EN",
-    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/6391518.jpg",
     top: 7,
     name: "Watson Amelia",
     creator: "Hololive EN",
-    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/4858745.jpg",
     top: 8,
     name: "Minato Aqua",
     creator: "Hololive JP",
-    point: "1000"
   },
 ];
 
