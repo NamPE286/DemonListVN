a9f7bdc01a1ce898cb3ce9ea3de5737f93fbb3fa
NamPE286 <nambuihung654@gmail.com>
2022-02-10 22:31:42 +0700

    update pt

--- pages/components/Main/Mainlist.js
+++ pages/components/Main/Mainlist.js
@@ -1,4 +1,5 @@
 function Main(levels) {
+  console.log(levels);
   return (
     <div className="mainpanel">
       <h2>Main List</h2>
@@ -35,41 +36,49 @@ Main.defaultProps = [
     thumbnail: "https://i.imgur.com/BdBy1Ky.png",
     name: "Rushia",
     creator: "Hololive JP",
+    point: "1000"
   },
   {
     thumbnail: "https://c4.wallpaperflare.com/wallpaper/262/965/121/gawr-gura-anime-girls-water-hd-wallpaper-preview.jpg",
     name: "Gawr Gura",
     creator: "Hololive EN",
+    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/6391518.jpg",
     name: "Watson Amelia",
     creator: "Hololive EN",
+    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/4858745.jpg",
     name: "Minato Aqua",
     creator: "Hololive JP",
+    point: "1000"
   },
   {
     thumbnail: "https://i.imgur.com/BdBy1Ky.png",
     name: "Rushia",
     creator: "Hololive JP",
+    point: "1000"
   },
   {
     thumbnail: "https://c4.wallpaperflare.com/wallpaper/262/965/121/gawr-gura-anime-girls-water-hd-wallpaper-preview.jpg",
     name: "Gawr Gura",
     creator: "Hololive EN",
+    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/6391518.jpg",
     name: "Watson Amelia",
     creator: "Hololive EN",
+    point: "1000"
   },
   {
     thumbnail: "https://wallpaperaccess.com/full/4858745.jpg",
     name: "Minato Aqua",
     creator: "Hololive JP",
+    point: "1000"
   },
 ];
 
