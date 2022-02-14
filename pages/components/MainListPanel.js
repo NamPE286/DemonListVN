4a6d4a1f49917d63252b05f95f9502a82a552ec5
NamPE286 <nambuihung654@gmail.com>
2022-02-14 10:48:25 +0700

    made the code prettier

--- pages/components/MainListPanel.js
+++ pages/components/MainListPanel.js
@@ -19,27 +19,27 @@ function Main(levels) {
       <section title={`${levels[0].name} by ${levels[0].creator} - ${levels[0].points}pt`}>
         <img src={`https://i.ytimg.com/vi/${levels[0].thumbnail}/hqdefault.jpg`} alt=''></img>
         <div className="fadeEffect1"></div>
-        <a className="smalltop">#1</a><a><div id="bold">{processTitle(levels[0].name)}</div><div>by {processAuthor(levels[0].creator)} - {levels[0].points}pt</div></a>
+        <a className="smalltop">#1</a><a>{`${processTitle(levels[0].name)} by ${processAuthor(levels[0].creator)} - ${levels[0].points}pt`}</a>
       </section>
       <section title={`${levels[1].name} by ${levels[1].creator} - ${levels[1].points}pt`}>
         <img src={`https://i.ytimg.com/vi/${levels[1].thumbnail}/hqdefault.jpg`} alt=''></img>
         <div className="fadeEffect1"></div>
-        <a className="smalltop">#2</a><a><div id="bold">{processTitle(levels[1].name)}</div><div>by {processAuthor(levels[1].creator)} - {levels[1].points}pt</div></a>
+        <a className="smalltop">#2</a><a>{`${processTitle(levels[1].name)} by ${processAuthor(levels[1].creator)} - ${levels[1].points}pt`}</a>
       </section>
       <section title={`${levels[2].name} by ${levels[2].creator} - ${levels[2].points}pt`}>
         <img src={`https://i.ytimg.com/vi/${levels[2].thumbnail}/hqdefault.jpg`} alt=''></img>
         <div className="fadeEffect1"></div>
-        <a className="smalltop">#3</a><a><div id="bold">{processTitle(levels[2].name)}</div><div>by {processAuthor(levels[2].creator)} - {levels[2].points}pt</div></a>
+        <a className="smalltop">#3</a><a>{`${processTitle(levels[2].name)} by ${processAuthor(levels[2].creator)} - ${levels[2].points}pt`}</a>
       </section>
       <section title={`${levels[3].name} by ${levels[3].creator} - ${levels[3].points}pt`}>
         <img src={`https://i.ytimg.com/vi/${levels[3].thumbnail}/hqdefault.jpg`} alt=''></img>
         <div className="fadeEffect1"></div>
-        <a className="smalltop">#4</a><a><div id="bold">{processTitle(levels[3].name)}</div><div>by {processAuthor(levels[3].creator)} - {levels[3].points}pt</div></a>
+        <a className="smalltop">#4</a><a>{`${processTitle(levels[3].name)} by ${processAuthor(levels[3].creator)} - ${levels[3].points}pt`}</a>
       </section>
       <section title={`${levels[4].name} by ${levels[4].creator} - ${levels[4].points}pt`}>
         <img src={`https://i.ytimg.com/vi/${levels[4].thumbnail}/hqdefault.jpg`} alt=''></img>
         <div className="fadeEffect1"></div>
-        <a className="smalltop">#5</a><a><div id="bold">{processTitle(levels[4].name)}</div><div>by {processAuthor(levels[4].creator)} - {levels[4].points}pt</div></a>
+        <a className="smalltop">#5</a><a>{`${processTitle(levels[4].name)} by ${processAuthor(levels[4].creator)} - ${levels[4].points}pt`}</a>
       </section>
     </div>
   )
