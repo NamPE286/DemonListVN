f151302cf7c93fa55de25f5d5b0b8055a91b523c
rVnPower <vnpower@ctemplar.com>
2022-02-20 10:17:29 +0700

    commit bug

---
+++ pages/components/Footer.js
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="footerTitle">
+        <h3>Made from love by GDVN developers</h3>
+        <h3>with inspiration from osu.ppy.sh and pointercrate.com</h3>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ pages/players/[id].js
@@ -0,0 +1,9 @@
+// Player object
+// name: String
+// id: Integer
+// position: Integer
+// total_points: Integer
+// demons: List[Demon]
+// position_history: Object {date, position}
+
+function Player()
