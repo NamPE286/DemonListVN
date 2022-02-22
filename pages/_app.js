6b1c34b400750a205ec6e3272dd49822effcbdd6
NamPE286 <nambuihung654@gmail.com>
2022-02-22 17:02:02 +0700

    added loading animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.scss'
 import Navbar from './components/Navbar.js'
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }) {
   return (
