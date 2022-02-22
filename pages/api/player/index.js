eaae81530154cc4e27669a260613b1d3ae709534
rVnPower <vnpower@ctemplar.com>
2022-02-22 17:30:21 +0700

    api base update

--- pages/api/player/index.js
+++ pages/api/player/index.js
@@ -1,10 +1,21 @@
 import nc from 'next-connect';
 import players from '../../../public/players.js'
-// We will connect to a database in production
 
-
-// This is a mock API to get players
-// Return all players
+// Player object
+// name: String
+// uid: Integer                             | Pre-calculated
+// position: Integer
+// point: Float
+// id: Integer
+// position_history: Object{date: position} | Pre-calculated, No return
+// points_history: Object{date: point}      | Pre-calculated, No return
+// mainlist_demons: List[Demon]
+// legacylist_demons: List[Demon]
+// best_demons: List[Demon]                 | Pre-calculated, No return
+//
+// 'date' values will be stored as unix milis
+// 'uid' is an unique ID, represents a player.
+// 'id' is the player ID on the GD server.
 
 const handler = nc()
   .get((req, res) => {
