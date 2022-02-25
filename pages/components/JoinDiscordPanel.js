23565e15c69d0a19e8c26f9af7c93795e07f0bde
NamPE286 <nambuihung654@gmail.com>
2022-02-25 16:44:21 +0700

    update style

--- pages/components/JoinDiscordPanel.js
+++ pages/components/JoinDiscordPanel.js
@@ -1,7 +1,7 @@
 function Main() {
     return (
         <div className='topplayerPanel'>
-            <iframe id="discord" src="https://discord.com/widget?id=877546680801697813&theme=dark" width="100%" height="200" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            <iframe id="discord" src="https://discord.com/widget?id=877546680801697813&theme=dark" width="100%" height="300" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </div>
     )
 }
