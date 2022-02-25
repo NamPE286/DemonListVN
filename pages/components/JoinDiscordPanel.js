544eb04e138a3813e513d48503c01eca142df3e2
NamPE286 <nambuihung654@gmail.com>
2022-02-25 15:47:18 +0700

    update side panel

--- pages/components/JoinDiscordPanel.js
+++ pages/components/JoinDiscordPanel.js
@@ -2,8 +2,7 @@ function Main() {
     return (
         <div className='topplayerPanel'>
             <h2>Join Our Discord Server</h2>
-            <iframe src="https://discord.com/widget?id=870137936010694676&theme=dark" width="310" height="350" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
-        </div>
+            <iframe src="https://discord.com/widget?id=877546680801697813&theme=dark" width="310" height="300" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>        </div>
     )
 }
 
