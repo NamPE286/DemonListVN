e78c5e783ac7eae6a02302a4f333d86fe2dc8ef8
NamPE286 <nambuihung654@gmail.com>
2022-02-27 14:21:40 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="App">
       <title>Demon List VN</title>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4605218533506777"
+     crossorigin="anonymous"></script>
       <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no" />
       <Navbar />
       <Component {...pageProps} />
