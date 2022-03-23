557295fe4e9a3377a07a7853674394f15849598f
NamPE286 <nambuihung654@gmail.com>
2022-03-23 14:35:02 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,9 @@ import Main from './components/Main/Mainlist.js';
 function App() {
   return (
     <>
+      <head>
+        <meta name="google-site-verification" content="X16WBQlc0YO_vA6usdN_KKfX0viP5rG8QVpgN497l84" />
+      </head>
       <Navbar />
       <title>Main List - Demon List VN</title>
       <div className='pageContent'>
