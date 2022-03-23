515390c7ddb31bce300447dd3a479952ff5434e6
NamPE286 <nambuihung654@gmail.com>
2022-03-23 14:39:40 +0700

    seo

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import AnnoucementPanel from './components/AnnoucementPanel.js';
 import JoinDiscordPanel from './components/JoinDiscordPanel.js';
 import Navbar from './components/Navbar.js';
 import Main from './components/Main/Mainlist.js';
+import Head from 'next/head';
 function App() {
   return (
     <>
