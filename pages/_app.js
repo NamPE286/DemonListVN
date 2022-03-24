0a5b49e5097b83551723ff46014450536b864102
NamPE286 <nambuihung654@gmail.com>
2022-03-24 16:34:55 +0700

    ad

--- pages/_app.js
+++ pages/_app.js
@@ -24,8 +24,8 @@ function MyApp({ Component, pageProps }) {
       <Helmet>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4605218533506777"
           crossOrigin="anonymous"></script>
+          <script src="https://itweepinbelltor.com/pfe/current/tag.min.js?z=4969814" data-cfasync="false" async></script>
       </Helmet>
-      
       <meta name="viewport" content="width=device-width, initial-scale=0.8, user-scalable=no" />
       <meta name="description" content="Welcome to Demon List VN, this is where we keep track of the 50 hardest demons created and verified by Vietnamese and other Vietnamese players managed to beat one of those levels!" />
       <Component {...pageProps} />
