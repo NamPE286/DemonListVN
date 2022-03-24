ad54925d9154424afeec2231d1f75f008067870f
NamPE286 <nambuihung654@gmail.com>
2022-03-24 14:55:21 +0700

    quit using youtube embed

--- pages/components/Main/Mainlist.js
+++ pages/components/Main/Mainlist.js
@@ -57,13 +57,9 @@ function Main() {
                         </a>
                         <div>
                           <div className="lvinfo" id={`lvf${i}`}>
-                            <iframe
-                              src={`https://www.youtube.com/embed/${data[1][i].thumbnail}&autoplay=1`}
-                              srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${data[1][i].thumbnail}?autoplay=1><span>▶</span></a>`}
-                              frameBorder="0"
-                              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                              allowFullScreen
-                            ></iframe>
+                            <div className="ytVid">
+                            <a href={`https://www.youtube.com/watch?v=${data[1][i].thumbnail}`} target="_blank"><span>▶</span></a>
+                            </div>
                             <div className="verticalLine"></div>
                             <div className="lvinfoContent">
                               <a><b>ID: </b>{data[1][i].id}</a><br></br>
