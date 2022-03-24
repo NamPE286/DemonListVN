c2d88a741e43ab362b43fc8425d1e02afd9d84e4
NamPE286 <nambuihung654@gmail.com>
2022-03-24 13:34:51 +0700

    deploy fix

--- pages/components/Main/Mainlist.js
+++ pages/components/Main/Mainlist.js
@@ -58,10 +58,10 @@ function Main() {
                               width="560"
                               height="315"
                               src={`https://www.youtube.com/embed/${data[1][i].thumbnail}&autoplay=1`}
-                              srcdoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${data[1][i].thumbnail}?autoplay=1><img src=https://img.youtube.com/vi/${data[1][i].thumbnail}/hqdefault.jpg><span>▶</span></a>`}
-                              frameborder="0"
+                              srcDoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${data[1][i].thumbnail}?autoplay=1><img src=https://img.youtube.com/vi/${data[1][i].thumbnail}/hqdefault.jpg><span>▶</span></a>`}
+                              frameBorder="0"
                               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                              allowfullscreen
+                              allowFullScreen
                             ></iframe>
                             <div className="verticalLine"></div>
                             <div className="lvinfoContent">
