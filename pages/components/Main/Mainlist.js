dbf3123194679891974929b679d8f90efd766483
NamPE286 <nambuihung654@gmail.com>
2022-03-24 13:31:53 +0700

    youtube iframe lazy load

--- pages/components/Main/Mainlist.js
+++ pages/components/Main/Mainlist.js
@@ -54,7 +54,15 @@ function Main() {
                         </div>
                         <div>
                           <div className="lvinfo" id={`lvf${i}`}>
-                            <iframe src={`https://www.youtube-nocookie.com/embed/${data[1][i].thumbnail}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                            <iframe
+                              width="560"
+                              height="315"
+                              src={`https://www.youtube.com/embed/${data[1][i].thumbnail}&autoplay=1`}
+                              srcdoc={`<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/${data[1][i].thumbnail}?autoplay=1><img src=https://img.youtube.com/vi/${data[1][i].thumbnail}/hqdefault.jpg><span>▶</span></a>`}
+                              frameborder="0"
+                              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                              allowfullscreen
+                            ></iframe>
                             <div className="verticalLine"></div>
                             <div className="lvinfoContent">
                               <a><b>ID: </b>{data[1][i].id}</a><br></br>
