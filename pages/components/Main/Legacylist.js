b46569421ab9732bccfd5350fddd39507319be6d
NamPE286 <nambuihung654@gmail.com>
2022-03-31 23:52:03 +0700

    ads thing

--- pages/components/Main/Legacylist.js
+++ pages/components/Main/Legacylist.js
@@ -1,7 +1,8 @@
-import {getDoc, doc} from "firebase/firestore"
+import { getDoc, doc } from "firebase/firestore"
 import { db } from '../../api/firebase-config.js'
 import { useState, useEffect } from 'react';
 import Image from "next/image";
+import Head from "next/head";
 
 function Main() {
   const [data, setData] = useState([]);
@@ -25,31 +26,60 @@ function Main() {
 
   // Subsequent queries will use persistence, if it was enabled successfully
   return (
-    <div className="mainpanel" data-aos="fade-up" data-aos-duration="800">
-      <h2>Legacy List</h2>
-      <div className="mainpanelContent">
-        {Object.keys(data).map(i => {
-          if (data[i].name != null) {
-            return (
-              <div className="levelWrapper" key={i}>
-                <a href={`/mainlist/${data[i].id}`}>
-                  <div className='levelCard'>
-                    <Image src={`https://i.ytimg.com/vi/${data[i].thumbnail}/hqdefault.jpg`} alt="" layout="fill" objectFit='cover' priority='true' quality={35}></Image>
-                    <div className='fadeEffect'></div>
-                    <div className='levelInfo'>
-                      <h3>{data[i].name}</h3>
-                      <p>by {data[i].creator}</p>
-                    </div>
+    <>
+      <Head>
+        <script type='text/javascript' src='//pl17157169.safestgatetocontent.com/ff/57/6c/ff576c334835b26609495b1e5cc64180.js'></script>
+        <script async="async" data-cfasync="false" src="//pl17157193.safestgatetocontent.com/ad80a2f0f05651f1a5916edbc7b4e0ab/invoke.js"></script>
+      </Head>
+      <div className="mainpanel" data-aos="fade-up" data-aos-duration="800">
+        <h2>Legacy List</h2>
+        <div className="mainpanelContent">
+          {Object.keys(data).map(i => {
+            if (data[i].name != null) {
+              if (i == 0) {
+                return (
+                  <>
+                  <div className="levelWrapper" key={i}>
+                    <a href={`/mainlist/${data[i].id}`}>
+                      <div className='levelCard'>
+                        <Image src={`https://i.ytimg.com/vi/${data[i].thumbnail}/hqdefault.jpg`} alt="" layout="fill" objectFit='cover' priority='true' quality={35}></Image>
+                        <div className='fadeEffect'></div>
+                        <p className='top'>#{parseInt(i) + 1}</p>
+                        <div className='levelInfo'>
+                          <h3>{data[i].name}</h3>
+                          <p>by {data[i].creator}</p>
+                        </div>
+                      </div>
+                    </a>
                   </div>
-                </a>
-              </div>
+                  <div id="container-ad80a2f0f05651f1a5916edbc7b4e0ab"></div>
+                  </>
 
-            )
+                )
+              }
+              else {
+                return (
+                  <div className="levelWrapper" key={i}>
+                    <a href={`/mainlist/${data[i].id}`}>
+                      <div className='levelCard'>
+                        <Image src={`https://i.ytimg.com/vi/${data[i].thumbnail}/hqdefault.jpg`} alt="" layout="fill" objectFit='cover' priority='true' quality={35}></Image>
+                        <div className='fadeEffect'></div>
+                        <p className='top'>#{parseInt(i) + 1}</p>
+                        <div className='levelInfo'>
+                          <h3>{data[i].name}</h3>
+                          <p>by {data[i].creator}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
 
-          }
-        })}
+                )
+              }
+            }
+          })}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- pages/components/Main/Mainlist.js
+++ pages/components/Main/Mainlist.js
@@ -29,31 +29,56 @@ function Main() {
     <>
       <Head>
         <script type='text/javascript' src='//pl17157169.safestgatetocontent.com/ff/57/6c/ff576c334835b26609495b1e5cc64180.js'></script>
+        <script async="async" data-cfasync="false" src="//pl17157193.safestgatetocontent.com/ad80a2f0f05651f1a5916edbc7b4e0ab/invoke.js"></script>
       </Head>
       <div className="mainpanel" data-aos="fade-up" data-aos-duration="800">
         <h2>Main List</h2>
         <div className="mainpanelContent">
           {Object.keys(data).map(i => {
             if (data[i].name != null) {
-              return (
-                <div className="levelWrapper" key={i}>
-                  <a href={`/mainlist/${data[i].id}`}>
-                    <div className='levelCard'>
-                      <Image src={`https://i.ytimg.com/vi/${data[i].thumbnail}/hqdefault.jpg`} alt="" layout="fill" objectFit='cover' priority='true' quality={35}></Image>
-                      <div className='fadeEffect'></div>
-                      <p className='top'>#{parseInt(i) + 1}</p>
-                      <div className='levelInfo'>
-                        <h3>{data[i].name}</h3>
-                        <p>by {data[i].creator}</p>
-                        <br />
-                        <p>{data[i].points}pt</p>
+              if (i == 0) {
+                return (
+                  <>
+                  <div className="levelWrapper" key={i}>
+                    <a href={`/mainlist/${data[i].id}`}>
+                      <div className='levelCard'>
+                        <Image src={`https://i.ytimg.com/vi/${data[i].thumbnail}/hqdefault.jpg`} alt="" layout="fill" objectFit='cover' priority='true' quality={35}></Image>
+                        <div className='fadeEffect'></div>
+                        <p className='top'>#{parseInt(i) + 1}</p>
+                        <div className='levelInfo'>
+                          <h3>{data[i].name}</h3>
+                          <p>by {data[i].creator}</p>
+                          <br />
+                          <p>{data[i].points}pt</p>
+                        </div>
                       </div>
-                    </div>
-                  </a>
-                </div>
+                    </a>
+                  </div>
+                  <div id="container-ad80a2f0f05651f1a5916edbc7b4e0ab"></div>
+                  </>
 
-              )
+                )
+              }
+              else {
+                return (
+                  <div className="levelWrapper" key={i}>
+                    <a href={`/mainlist/${data[i].id}`}>
+                      <div className='levelCard'>
+                        <Image src={`https://i.ytimg.com/vi/${data[i].thumbnail}/hqdefault.jpg`} alt="" layout="fill" objectFit='cover' priority='true' quality={35}></Image>
+                        <div className='fadeEffect'></div>
+                        <p className='top'>#{parseInt(i) + 1}</p>
+                        <div className='levelInfo'>
+                          <h3>{data[i].name}</h3>
+                          <p>by {data[i].creator}</p>
+                          <br />
+                          <p>{data[i].points}pt</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
 
+                )
+              }
             }
           })}
         </div>
