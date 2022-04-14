3e03b473bbb04d050c00d8acb2eb8c1065ba3771
NamPE286 <nambuihung654@gmail.com>
2022-04-14 19:01:30 +0700

    combine submit page

--- pages/components/Main/Submit.js
+++ pages/components/Main/Submit.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
 function Main() {
+    const [sel, setSel] = useState(0);
 
+    function showGGF() {
+        if (sel == 0) {
+            return (
+                <>
+                    <div className="submitForm">
+                        <iframe className="google-form" src="https://docs.google.com/forms/d/e/1FAIpQLScyjv64Ezp9-3kQPUvZ-aCpEjKue9jhXKqVnq2bFSGqARpzUg/viewform?embedded=true" width="90%" height="1260" frameBorder="0" marginHeight="0" marginWidth="0"></iframe>
+                    </div>
+                    <p className="submitP">Doesn't work? <a href="https://forms.gle/YfRLRSrb1MPB8vqG7"><u>Click Here</u></a></p>
+
+                </>
+            )
+        }
+        else{
+            return (
+                <>
+                    <div className="submitForm">
+                        <iframe className="google-form" src="https://docs.google.com/forms/d/e/1FAIpQLScnysX6n-jJZJV-Y5acianEVF7hML-tEdGa78qTh6hX0C8gTg/viewform?embedded=true" width="90%" height="1260" frameBorder="0" marginHeight="0" marginWidth="0"></iframe>
+                    </div>
+                    <p className="submitP">Doesn't work? <a href="https://forms.gle/YfRLRSrb1MPB8vqG7"><u>Click Here</u></a></p>
+
+                </>
+            )
+        }
+    }
     return (
         <div className="mainpanel" data-aos="fade-up" data-aos-duration="600">
             <h2>Submit</h2>
@@ -11,10 +37,12 @@ function Main() {
                     <li>Stealing videos from others will not be accepted.</li>
                 </ul>
             </div>
-            <div className="submitForm" data-aos="fade-up" data-aos-duration="600">
-                <iframe className="google-form" src="https://docs.google.com/forms/d/e/1FAIpQLScyjv64Ezp9-3kQPUvZ-aCpEjKue9jhXKqVnq2bFSGqARpzUg/viewform?embedded=true" width="90%" height="1260" frameBorder="0" marginHeight="0" marginWidth="0"></iframe>
+            <div className="submitSelect">
+                <a href="#!" onClick={() => setSel(0)}>Submit to VNFDL</a>
+                <a href="#!" onClick={() => setSel(1)}>Submit to GDVNAL</a>
             </div>
-            <p className="submitP">Doesn't work? <a href="https://forms.gle/YfRLRSrb1MPB8vqG7"><u>Click Here</u></a></p>
+            <hr></hr>
+            {showGGF()}
         </div>
     )
 }
