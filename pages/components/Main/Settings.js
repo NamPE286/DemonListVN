210e0bfc9e31f3fa1f53112edc623a044f1d4775
NamPE286 <nambuihung654@gmail.com>
2022-04-21 19:18:56 +0700

    setting fix

--- pages/components/Main/Settings.js
+++ pages/components/Main/Settings.js
@@ -2,16 +2,19 @@ import { useTheme } from 'next-themes'
 
 function Main() {
   const { theme, setTheme } = useTheme()
+  console.log(theme)
+  function processThing(x) {
+    let v = JSON.parse(JSON.stringify(x))
+    v = v.charAt(0).toUpperCase() + v.slice(1)
+    return v
+  }
   return (
     <div className="mainpanel">
       <h2>Settings</h2>
       <div>
         <label for="themes">Theme: </label>
         <select name="themes" id="themes">
-          <option value={theme} selected disabled hidden>{() => {
-            theme[0] = theme[0].toUpperCase()
-            return theme
-          }}</option>
+          <option value={theme} selected disabled hidden>{processThing(theme)}</option>
           <option value="light">Light</option>
           <option value="dark">Dark</option>
         </select>
