f741d89ebe5bf75c434eef29721f2ad4206fbd7b
NamPE286 <nambuihung654@gmail.com>
2022-04-24 11:27:04 +0700

    remove unnnecessary import

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,11 @@
 import '../styles/globals.scss'
-import Navbar from './components/Navbar.js'
 import Router from 'next/router';
 import NProgress from 'nprogress'; //nprogress module
 import 'nprogress/nprogress.css'; //styles of nprogress
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
-import Helmet from 'react-helmet';
 import { ThemeProvider } from 'next-themes';
-import Script from 'next/script'
 
 // Import the functions you need from the SDKs you need
 
