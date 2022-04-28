7ad94783244f4fc8e0e14330ba38ad9c90ae22b5
NamPE286 <nambuihung654@gmail.com>
2022-04-28 15:23:13 +0700

    style update

--- pages/components/JoinDiscordPanel.js
+++ pages/components/JoinDiscordPanel.js
@@ -1,7 +1,10 @@
+import { useTheme } from 'next-themes'
+
 function Main() {
+    const { theme, setTheme } = useTheme()
     return (
         <div className='topplayerPanel'>
-            <iframe id="discord" src="https://discord.com/widget?id=877546680801697813&theme=dark" width="100%" height="300" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+            <iframe id="discord" src={`https://discord.com/widget?id=877546680801697813&theme=${theme}`} width="100%" height="300" allowtransparency="true" frameBorder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </div>
     )
 }
