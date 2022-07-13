75232a5077128d5260f028a21f185e6b13c33001
NamPE286 <nambuihung654@gmail.com>
2022-07-13 23:55:04 +0700

    contest prototype

--- pages/contest/[id].js
+++ pages/contest/[id].js
@@ -1,11 +1,13 @@
 import { useRouter } from "next/router";
 import Head from 'next/head';
 import Navbar from '../components/Navbar.js';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-function Main(){
-    const { id } = useRouter().query;
+function Main() {
+    const router = useRouter();
+    const { id } = router.query;
     const [sel, setSel] = useState(0);
+    const [mode, setMode] = useState()
     var uName = ''
     if (typeof window !== 'undefined') {
         uName = localStorage.getItem('userName')
@@ -13,6 +15,11 @@ function Main(){
             uName = ''
         }
     }
+    useEffect(() => {
+        if (router.query.mode) {
+            setMode(router.query.mode)
+        }
+    }, [router.query.mode])
     if (sel == 0) {
         try {
             document.getElementById('ab').style.background = 'var(--a2)';
@@ -28,8 +35,44 @@ function Main(){
         }
         catch (err) { }
     }
-    function showContent(){
-        return(
+    function showSubmit() {
+        if (!mode) return (
+            <>
+                <div id='abcz'>
+                    <h3>Submit</h3>
+                </div>
+                <div className="submit">
+                    <label for='userName'>Your in-game name:</label><br></br>
+                    <input type='text' id='userName' name='userName' defaultValue={uName} /><br></br>
+                    <label for='lvID'>Level</label><br></br>
+                    <input type='number' id='lvID' name='lvID' /><br></br>
+                    <label for='device'>Device (refresh rate):</label><br></br>
+                    <select type='text' id='device' name='device'>
+                        <option value="60Hz">60Hz</option>
+                        <option value="144Hz">144Hz</option>
+                        <option value="240Hz">240Hz</option>
+                        <option value="300Hz">300Hz</option>
+                        <option value="300Hz">360Hz</option>
+                        <option value="Mobile 60Hz">Mobile 60Hz</option>
+                        <option value="Mobile 144Hz">Mobile 144Hz</option>
+                        <option value="Mobile 240Hz">Mobile 240Hz</option>
+                        <option value="Mobile 300Hz">Mobile 300Hz</option>
+                        <option value="Mobile 300Hz">Mobile 360Hz</option>
+                    </select><br></br>
+                    <label for='percent'>Progress:</label><br></br>
+                    <input type='number' min='0' max='100' id='percent' name='percent' /><br></br>
+                    <label for='link'>YouTube video's link:</label><br></br>
+                    <input type='text' id='link' name='link' /><br></br>
+                    <div className="submitBtn1">
+                        <button>Submit</button>
+                    </div>
+                </div>
+
+            </>
+        )
+    }
+    function showContent() {
+        return (
             <div className="mainpanel" id='center-div'>
                 <h2>Contest Name's info</h2>
                 <div className="mainpanelContent">
@@ -47,35 +90,7 @@ function Main(){
                             <section><label id='contestLevel'><b>Level Name</b> by <b>Creator</b></label><label id='contestLevelID'>12345678</label><label id='contestLevelDiff'>Extreme Demon</label></section>
                         </div>
                     </div>
-                    <div id='abcz'>
-                        <h3>Submit</h3>
-                    </div>
-                    <div className="submit">
-                        <label for='userName'>Your in-game name:</label><br></br>
-                        <input type='text' id='userName' name='userName' defaultValue={uName} /><br></br>
-                        <label for='lvID'>Level</label><br></br>
-                        <input type='number' id='lvID' name='lvID' /><br></br>
-                        <label for='device'>Device (refresh rate):</label><br></br>
-                        <select type='text' id='device' name='device'>
-                            <option value="60Hz">60Hz</option>
-                            <option value="144Hz">144Hz</option>
-                            <option value="240Hz">240Hz</option>
-                            <option value="300Hz">300Hz</option>
-                            <option value="300Hz">360Hz</option>
-                            <option value="Mobile 60Hz">Mobile 60Hz</option>
-                            <option value="Mobile 144Hz">Mobile 144Hz</option>
-                            <option value="Mobile 240Hz">Mobile 240Hz</option>
-                            <option value="Mobile 300Hz">Mobile 300Hz</option>
-                            <option value="Mobile 300Hz">Mobile 360Hz</option>
-                        </select><br></br>
-                        <label for='percent'>Progress:</label><br></br>
-                        <input type='number' min='0' max='100' id='percent' name='percent' /><br></br>
-                        <label for='link'>YouTube video's link:</label><br></br>
-                        <input type='text' id='link' name='link' /><br></br>
-                        <div className="submitBtn1">
-                            <button>Submit</button>
-                        </div>
-                    </div>
+                    {showSubmit()}
                     <div id='abcz1'>
                         <h3>Standing</h3>
                     </div>
@@ -83,20 +98,31 @@ function Main(){
                         <a href="#!" id="ab" className="xzy" onClick={() => setSel(0)} style={{ background: "var(--a2)" }}>Accepted</a>
                         <a href="#!" id="ac" className="xzy" onClick={() => setSel(1)}>All Submission</a>
                     </div>
+                    <div className="allPlayer">
+                        <section>
+                            <a id="playerName"></a><a id="playerTotalPoint">Rating</a><a id="playerBestplay">Status</a>
+                        </section>
+                        <section className="allPlayerInfo">
+                            <a id="playerName" href='#!'>#1 Player Name</a><a id="playerTotalPoint">1000</a><a id="playerBestplay">Accepted</a>
+                        </section>
+                        <section className="allPlayerInfo">
+                            <a id="playerName" href='#!'>#2 Player Name</a><a id="playerTotalPoint">1000</a><a id="playerBestplay">Accepted</a>
+                        </section>
+                    </div>
                 </div>
             </div>
         )
     }
     return (
         <>
-        <Head>
-          <title>{id} - Demon List VN</title>
-        </Head>
-        <Navbar />
-        <div className='pageContent'>
-          {showContent()}
-        </div>
-      </>
+            <Head>
+                <title>{id} - Demon List VN</title>
+            </Head>
+            <Navbar />
+            <div className='pageContent'>
+                {showContent()}
+            </div>
+        </>
     )
 }
 export default Main
